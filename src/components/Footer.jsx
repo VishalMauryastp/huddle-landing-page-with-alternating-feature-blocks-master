1eb759d65c6cf1be816de03972c95a75d909cbfd
Vishal Maurya <vishalmaurya12nov@gmil.com>
2023-10-19 12:53:19 +0530

    footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -102,17 +102,7 @@ const Footer = () => {
         <p className="right">
           &copy; Copyright 2023 Huddle. All rights reserved.
         </p>
-        <p className="attribution">
-          Challenge by{" "}
-          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-            Frontend Mentor
-          </a>
-          . Coded by{" "}
-          <a href="https://github.com/VikashMaurya10/VikashMaurya10">
-            Vikash Maurya
-          </a>
-          .
-        </p>
+        
       </div>
     </footer>
   );
